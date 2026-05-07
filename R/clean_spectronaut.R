5129c7780b1760ac864d5a958e4f44667a01ad47
Tony Wu <wu.anthon@northeastern.edu>
2026-05-07 14:50:29 -0400

    use col_names parameter

--- R/clean_spectronaut.R
+++ R/clean_spectronaut.R
@@ -43,7 +43,7 @@ reduceBigSpectronaut <- function(input_file, output_path,
                             readr::DataFrameCallback$new(spec_chunk),
                             delim = delim,
                             chunk_size = 1e6,
-                            col_select = tidyselect::any_of(needed_cols))
+                            col_names = tidyselect::any_of(needed_cols))
 }
 
 #' @keywords internal
